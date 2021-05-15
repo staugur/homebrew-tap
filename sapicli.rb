218372687f216435cc01c1382addf154011c8327
Hiroshi.tao <me@tcw.im>
2021-05-15 21:05:23 +0800

    update sapic version

--- sapicli.rb
+++ sapicli.rb
@@ -3,8 +3,8 @@
 class Sapicli < Formula
   desc "sapic client cli"
   homepage "https://github.com/sapicd/cli"
-  url "https://static.saintic.com/download/picbed-cli/sapicli.0.5.0-darwin-amd64.tar.gz"
-  sha256 "f2deb3b59b46aa57958956dc54f2c995d3df45f28a1f543af4f7adb312d5190c"
+  url "https://static.saintic.com/download/sapicli/sapicli.0.5.2-darwin-amd64.tar.gz"
+  sha256 "c2db48e3cfde6637840c4df28689d127acc4f0919e2ede50fef0d8879c672ba4"
 
   # depends_on "cmake" => :build
 
